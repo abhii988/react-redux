07aa14fa3c8b93948cbae6c0be4a1e1c9e87eb65
Abhishek Gupta <70751569+abhii988@users.noreply.github.com>
2022-08-31 10:47:02 +0545

    Inital commit

---
+++ src/components/Nav.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { NavLink, Outlet } from "react-router-dom";
+
+const Nav = ({ Logout }) => {
+  // const user = localStorage.getItem("username");
+  const navStyle = {
+    color: "whitesmoke",
+    textDecoration: "none",
+  };
+  return (
+    <>
+      <nav>
+        <NavLink activeClassName="active" style={navStyle} to="/">
+          <h3>Logo</h3>
+        </NavLink>
+        <ul className="nav-links">
+          <NavLink activeClassName="active" style={navStyle} to="/about">
+            <li>About Page</li>
+          </NavLink>
+          <NavLink activeClassName="active" style={navStyle} to="/shop">
+            <li>Users Page</li>
+          </NavLink>
+          <NavLink activeClassName="active" style={navStyle} to={`/dashboard`}>
+            <li>Dashboard</li>
+          </NavLink>
+          <NavLink activeClassName="active" style={navStyle} to={`/profile`}>
+            <li>Profile</li>
+          </NavLink>
+          <li>
+            <Button variant="outline-light" value="LOGOUT" onClick={Logout}>
+              Logout
+            </Button>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Nav;
